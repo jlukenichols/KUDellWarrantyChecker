f21507cd6222ead58cc5b2689ef60457c994067a
jlukenichols <J.Luke.Nichols@gmail.com>
2021-05-11 15:03:26 -0400

    Built out more of the script and added example input file
    Built out more of the script and added example input file

--- DefaultSettings.ps1
+++ DefaultSettings.ps1
@@ -1,6 +1,9 @@
+#You can modify this file or create your own CustomSettings.ps1 file.
+#If CustomSettings.ps1 exists then it will be read instead of this file.
+
 #Set variables for Write-Log function
-$LoggingMode = $true
-$VerboseLogging = $true
+$LoggingMode = $true #Setting this to $false will prevent all logging
+$VerboseLogging = $true #Setting this to $true will output log strings to both the console and the log file
 
 #Define the root path of the running script
 $myPSScriptRoot = "C:\Scripts\WakeOnLANFromCSV"
@@ -8,8 +11,18 @@ $myPSScriptRoot = "C:\Scripts\WakeOnLANFromCSV"
 #Define the path to the log file
 [string]$LogFilePath = "$($myPSScriptRoot)\logs\WakeOnLANFromCSV_$($currentYear)-$($currentMonth)-$($currentDay)T$($currentHour)$($currentMinute)$($currentSecond)_$($env:computername).log"
 
-#Define the path to the CSV file containing the MAC addresses. Assumes the first line is the header line and that there is a "MACAddress" field.
-$FullPathToCSV = "\\file.server\share\serviceTags.csv"
+#Define the path to the CSV file containing the Dell service tags.
+#Assumes the first line is the header line, there is a "Computer Name" field, and there is a "Computer Serial Number" field containing the Dell service tag.
+#Can contain extra fields but they will be ignored.
+#Can be a UNC path.
+$FullPathToInputCSV = "$($myPSScriptRoot)\ExampleInput.CSV"
 
-#Define your Dell warranty API key.
-$DellWarrantyAPIKey = "" # Get your own from Dell TechDirect https://techdirect.dell.com/Portal/APIs.aspx
+#Define the path to the CSV file where you will output the warranty data.
+#Can be a UNC path.
+$FullPathToOutputCSV = "$($myPSScriptRoot)\DellWarrantyData.CSV"
+
+#Define your Dell warranty API project name. This will be the "client_id" when you generate your token.
+$DellWarrantyProjectName = "" # Create your own in Dell TechDirect https://techdirect.dell.com/Portal/APIs.aspx If you run into issues email APIs_TechDirect@dell.com
+
+#Define your Dell warranty API key. This will be the "client_id" when you generate your token.
+$DellWarrantyAPIKey = "" # Get your own from Dell TechDirect https://techdirect.dell.com/Portal/APIs.aspx If you run into issues email APIs_TechDirect@dell.com
--- KUDellWarrantyChecker.ps1
+++ KUDellWarrantyChecker.ps1
@@ -10,7 +10,7 @@ Clear-Host
 
 .NOTES
     Release Date: 2021-05-11T13:01
-    Last Updated: 2021-05-11T13:01
+    Last Updated: 2021-05-11T14:04
    
     Author: Luke Nichols
     Github link: https://github.com/jlukenichols/KUDellWarrantyChecker
@@ -21,8 +21,49 @@ Clear-Host
 
 #-------------------------- Begin defining functions --------------------------
 
-# Dot-source function for writing to log file
+# Dot-source functions for writing to log files
 . .\functions\Write-Log.ps1
+
+# Function for generating Dell API auth token
+Function Get-AuthToken {
+    #Mostly taken from https://www.undocumented-features.com/2020/06/30/powershell-oauth-authentication-two-ways/ but adapted to the Dell API
+    Param (
+        [string]$clientID,
+        [string]$clientSecret
+    )
+    $encodedSecret = [System.Web.HttpUtility]::UrlEncode($clientSecret)
+
+    $requestAccessTokenUri = "https://apigtwb2c.us.dell.com/auth/oauth/v2/token"
+    $body = "grant_type=client_credentials&client_id=$clientID&client_id=$encodedSecret"
+    $contentType = "application/x-www-form-urlencoded"
+    try {
+        $Token = Invoke-RestMethod -Method Post -Uri $requestAccessTokenUri -Body $body -ContentType $contentType
+        $script:AuthenticationResult = Get-AuthToken -applicationId $ClientId -secret $ClientSecret
+    }
+    catch {
+        throw
+    }
+}
+
+# Function for retrieving Dell warranty data using token from Get-AuthToken
+Function Retrieve-WarrantyData {
+    Param (
+        [string]$AuthToken,
+        [array]$DellSvcTags
+    )
+
+    $warrantyCheckUri = "https://apigtwb2c.us.dell.com/PROD/sbil/eapi/v5/asset-entitlements"
+    $headers = @{
+        'Authorization' = $AuthToken
+    }
+    $body = $DellSvcTags
+
+    try {
+        $WarrantyEndDate = Invoke-RestMethod -Method GET -Uri $warrantyCheckUri -Headers $headers -Body $body
+    } catch {
+        throw
+    }
+}
 
 #-------------------------- End defining functions --------------------------
 
@@ -39,16 +80,6 @@ $currentHour = $($currentDate.Hour).ToString("00")
 $currentMinute = $($currentDate.Minute).ToString("00")
 $currentSecond = $($currentDate.Second).ToString("00")
 
-#Set variables for write-log function
-$LoggingMode = $true
-$VerboseLogging = $true
-
-#Define the root path of the running script
-$myPSScriptRoot = "C:\Scripts\WakeOnLANFromCSV"
-
-#Define the path to the log file
-[string]$LogFilePath = "$($myPSScriptRoot)\logs\KUDellWarrantyChecker_$($currentYear)-$($currentMonth)-$($currentDay)T$($currentHour)$($currentMinute)$($currentSecond)_$($env:computername).log"
-
 #Dot-source settings file
 if (Test-Path .\CustomSettings.ps1) {
     . .\CustomSettings.ps1
@@ -58,11 +89,62 @@ if (Test-Path .\CustomSettings.ps1) {
     $LogMessage = "Importing settings from DefaultSettings.ps1"
 }
 
-#Define the path to the CSV file containing the MAC addresses. Assumes the first line is the header line and that there is a "MACAddress" field.
-#$FullPathToCSV = "\\kuit.ku.kettering.edu\itstuff\Software\PDQ_Deploy_Repo\Reports\MacAddrOn620\MAC Addresses on 620 VLAN.csv"
-
 #-------------------------- End setting initial values --------------------------
 
 #-------------------------- Start main script body --------------------------
 
+#TODO: Add code to generate Dell API bearer token using client_id and client_secret
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 # https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel
+Add-Type -AssemblyName System.Web #Required for the API lookups, not sure why. See https://www.undocumented-features.com/2020/06/30/powershell-oauth-authentication-two-ways/
+
+#Get auth token. Lasts for 3600 seconds (1 hour).
+$AuthenticationResult = Get-AuthToken -clientID $DellWarrantyProjectName -clientSecret $DellWarrantyAPIKey
+
+#Create CSV file object
+$InputCSVFile = Import-CSV $FullPathToInputCSV
+
+#Iterate through CSV file
+foreach ($line in $InputCSVFile) {
+    Write-Host "ComputerName: $($line."Computer Name") DellServiceTag: $($line."Computer Serial Number")"
+    #TODO: Write code to pull its warranty end date via Dell API for each service tag and dump it to an output CSV
+    Retrieve-WarrantyData -AuthToken $AuthenticationResult -DellSvcTags $($line."Computer Serial Number")
+    pause
+}
+
+#TODO: Write code to import data from output CSV into PDQ Inventory
+
 #-------------------------- End main script body --------------------------
+
+<# Info from email from Dell
+
+As we use OAuth2.0 as the authorization hence it’s a two-step process to access warranty endpoint(s): 
+
+1.                   Generate Bearer token using client_id and client_secret. The Bearer Token will be valid for 3600 seconds, you should generate token before it expires.
+
+Kindly refrain to generate token for every call once you move to production instance 
+
+HTTP POST Method ; URI : https://apigtwb2c.us.dell.com/auth/oauth/v2/token
+
+requestBody
+
+    grant_type = client_credentials
+
+    client_id = your client_id
+
+    client_secret = your client_secret
+
+                          Content-Type = application/x-www-form-urlencoded
+ 
+
+2.                   Use the Bearer token to make Warranty API call (new endpoints for warranty V5 API) wherein Bearer token should be passed as Header whilst servicetag(s) as query parameter - refer Warranty technical specs for more details
+
+HTTP GET Method -  URI :   https://apigtwb2c.us.dell.com/PROD/sbil/eapi/v5/asset-entitlements
+
+Headers
+
+Authorization = Bearer token
+
+Query Param (body) =
+                                            servicetags = tag(s) e.g., tag1,tag2, tag3…
+
+#>
